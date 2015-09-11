5331b74a37d99fe52080724245efe37c251c02f6
Matteo Migliore <matteo.migliore@gmail.com>
2015-09-11 16:32:49 +0000

    Net.Http - allow to post a file in streaming

---
+++ Main/Prototypes/Micro.Commanding/Service/Modules/Common/Source/Web/Controllers/FileResult.cs
@@ -0,0 +1,16 @@
+namespace EyeSoft.Accounting.Prototype.Api.Web.Controllers
+{
+	using System;
+	using System.Collections.Generic;
+
+	public class FileResult
+	{
+		public IEnumerable<string> FileNames { get; set; }
+		public string Description { get; set; }
+		public DateTime CreatedTimestamp { get; set; }
+		public DateTime UpdatedTimestamp { get; set; }
+		public string DownloadLink { get; set; }
+		public IEnumerable<string> ContentTypes { get; set; }
+		public IEnumerable<string> Names { get; set; }
+	}
+}
