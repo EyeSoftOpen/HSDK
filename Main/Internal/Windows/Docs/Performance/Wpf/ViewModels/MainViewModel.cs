336d768c16f6016e0caad75a68d16dd9608e1a76
Michele Vicentini <ing.vicentini@gmail.com>
2016-06-28 07:58:48 +0000

    Added example of command with ViewModel parameter

--- Main/Internal/Windows/Docs/Performance/Wpf/ViewModels/MainViewModel.cs
+++ Main/Internal/Windows/Docs/Performance/Wpf/ViewModels/MainViewModel.cs
@@ -3,22 +3,23 @@
 	using System.Collections.Generic;
 	using System.Linq;
 	using System.Threading.Tasks;
+	using System.Windows.Input;
 
 	using EyeSoft.Windows.Model;
 	using EyeSoft.Windows.Model.Collections.ObjectModel;
 
-	public class MainViewModel : ViewModel
+	public class MainViewModel : AutoRegisterViewModel
 	{
 		private readonly IObservableCollection<PartyItemViewModel> partyList = CollectionFactory.Create<PartyItemViewModel>();
 
 		public MainViewModel()
 		{
-			Task.Run(() => Enumerable.Range(1, 10000).ToList().ForEach(x => partyList.Add(new PartyItemViewModel(x, x * 2))));
+		    Task.Run(() => Enumerable.Range(1, 10000).ToList().ForEach(x => partyList.Add(new PartyItemViewModel(x, x * 2))));
 		}
 
 		public IEnumerable<PartyItemViewModel> PartyList
 		{
 			get { return partyList; }
 		}
-	}
+    }
 }
--- Main/Internal/Windows/Docs/Performance/Wpf/ViewModels/PartyItemViewModel.cs
+++ Main/Internal/Windows/Docs/Performance/Wpf/ViewModels/PartyItemViewModel.cs
@@ -1,8 +1,10 @@
 ﻿namespace EyeSoft.Docs.Performance.Wpf.ViewModels
 {
+	using System.Windows.Input;
+
 	using EyeSoft.Windows.Model;
 
-	public class PartyItemViewModel : ViewModel
+	public class PartyItemViewModel : AutoRegisterViewModel
 	{
 		public PartyItemViewModel(int value1, int value2)
 		{
@@ -22,9 +24,33 @@
 			private set { SetProperty(value); }
 		}
 
+        public Area Area
+        {
+            get { return GetProperty<Area>(); }
+            private set { SetProperty(value); }
+        }
+
+        public ICommand UpdateValueCommand { get; private set; }
+
 		public override string ToString()
 		{
 			return string.Concat(Value1, " - ", Value2);
 		}
+
+		public void UpdateValue(PartyItemViewModel value)
+		{
+            Area = value.Area;
+        }
+
+		public bool CanUpdateValue(PartyItemViewModel value)
+		{
+			return true;
+		}
+	}
+
+	public enum Area
+	{
+		Mastership,
+		Agenda
 	}
 }
